98139776d9ec454625021738e5c5a62ecfe0c593
Jan <jannaj02@protonmail.com>
2020-11-17 10:43:00 +0100

    rolled back to server side streaming

---
+++ unchat/message_forwarding.py
@@ -0,0 +1,6 @@
+def send_message_to(recipient_id: str):
+    pass
+
+
+def identify_client(client_id: str):
+    pass
--- unchat/server.py
+++ unchat/server.py
@@ -19,30 +19,21 @@ class ChatServer(rpc.ChatMessagesServicer):
         self.clients.append(str(request.senderID))
         return chat.RequestSuccess()
 
-    def ChatStream(self, request_iterator, context):
-        old_messages = []
-        for new_message in request_iterator:
-            for old_message in old_messages:
-                if old_message.location == new_message.location:
-                    yield old_message
-            old_messages.append(new_message)
-
-    """def ChatStream(self, request, context):
+    def ChatStream(self, request, context):
         last_index = 0
         while True:
             while len(self.chats) > last_index:
                 message = self.chats[last_index]
                 last_index += 1
                 print(message)
-                yield message"""
+                yield message
 
 
 if __name__ == "__main__":
     print("Starting chat server...\n")
-    port = 180320
+    port = 180302
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=8))
     rpc.add_ChatMessagesServicer_to_server(ChatServer(), server)
     server.add_insecure_port(f'[::]:{port}')
     server.start()
-    while True:
-        time.sleep(18 * 3 * 2002)
+    server.wait_for_termination()
