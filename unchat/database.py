95ecfa95babbc91a38eef044fc292b92d76f77ba
Jan <jannaj02@protonmail.com>
2020-11-24 09:04:02 +0100

    adjusted server.py and database.py to new proto classes

--- unchat/database.py
+++ unchat/database.py
@@ -1,5 +1,6 @@
 import pymysql.cursors
 import bcrypt
+import unchat.chat_message_pb2 as chat
 
 
 class DBConnector:
@@ -15,17 +16,40 @@ class DBConnector:
         user_list = self.cursor.fetchone()
         return user_list[0]
 
-    def insert_user(self, user):
-        sql_statement = "INSERT INTO Users" \
-                        "(user_id, user_name, password, created_at, status, biography, path_profile_picture)" \
-                        "VALUES" \
-                        "(%(id), %(name), %(password), %(created_at), %(status), %(biography), %(path_profile_picture))"
-        prepared_statements = {}  # class is not implemented in python yet
+    def get_user_by_name(self, user_name: str):
+        sql_statement = "SELECT * FROM Users WHERE user_name = %(name)"
+        prepared_statements = {"name": user_name}
         self.cursor().execute(sql_statement, prepared_statements)
+        user_list = self.cursor.fetchone()
+        return user_list[0]
+
+    def insert_user(self, user: chat.UserLogin) -> bool:
+        sql_statement = "INSERT INTO Users" \
+                        "(user_id, user_name, password)" \
+                        "VALUES" \
+                        "(%(id), %(name), %(password))"
+        # user_id
+        user_name = user.username
+        password = self.hash_password(user.password)
+        # rest is default or set later on
+
+        prepared_statements = {"id": "user_id", "name": user_name, "password": password}
+        try:
+            self.cursor().execute(sql_statement, prepared_statements)
+            return True
+        except Exception:
+            return False
 
     def get_password_by_user_id(self, user_id: str):
         sql_statement = "SELECT password FROM Users WHERE user_id = %(id)"
         prepared_statements = {"id": user_id}
+        self.cursor().execute(sql_statement, prepared_statements)
+        password_list = self.cursor.fetchone()
+        return password_list[0]
+
+    def get_password_by_user_name(self, user_name: str):
+        sql_statement = "SELECT password FROM Users WHERE user_name = %(name)"
+        prepared_statements = {"name": user_name}
         self.cursor().execute(sql_statement, prepared_statements)
         password_list = self.cursor.fetchone()
         return password_list[0]
@@ -35,8 +59,8 @@ class DBConnector:
         hashed_password = bcrypt.hashpw(bytes_password, bcrypt.gensalt())
         return hashed_password
 
-    def compare_passwords(self, given_password: str, user_id: str):
-        hashed_user_password = self.get_password_by_user_id(user_id)
+    def compare_passwords(self, given_password: str, user_name: str):
+        hashed_user_password = self.get_password_by_user_name(user_name)
         bytes_given_password = bytes(given_password, "utf-8")
 
         is_password_equal = bcrypt.checkpw(bytes_given_password, hashed_user_password)
