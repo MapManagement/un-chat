8b805f142aec2f307c12ad3966241757709692ea
Jan <jannaj02@protonmail.com>
2020-11-25 13:09:09 +0100

    added user login functionality

--- unchat/database.py
+++ unchat/database.py
@@ -1,4 +1,6 @@
-import pymysql.cursors
+from unchat import secrets
+
+import sqlalchemy as db
 import bcrypt
 import unchat.chat_message_pb2 as chat
 
@@ -6,52 +8,49 @@ import unchat.chat_message_pb2 as chat
 class DBConnector:
 
     def __init__(self):
-        self.connection = pymysql.connect(host="abc", user="abc", password="abc", db="Users", charset="utf8mb4")
-        self.cursor = self.connection.cursor()
+        self.cursor = db.create_engine(secrets.conn)
 
     def get_user_by_id(self, user_id: str):
-        sql_statement = "SELECT * FROM Users WHERE user_id = %(id)"
-        prepared_statements = {"id": user_id}
-        self.cursor().execute(sql_statement, prepared_statements)
-        user_list = self.cursor.fetchone()
-        return user_list[0]
+        sql_statement = "SELECT * FROM Users WHERE user_id = %s"
+        prepared_statements = (user_id,)
+        db_query = self.cursor.execute(sql_statement, prepared_statements)
+        user = db_query.fetchone()
+        return user
 
     def get_user_by_name(self, user_name: str):
-        sql_statement = "SELECT * FROM Users WHERE user_name = %(name)"
+        sql_statement = "SELECT * FROM Users WHERE user_name = %(name)s"
         prepared_statements = {"name": user_name}
-        self.cursor().execute(sql_statement, prepared_statements)
-        user_list = self.cursor.fetchone()
-        return user_list[0]
+        db_query = self.cursor.execute(sql_statement, prepared_statements)
+        user = db_query.fetchone()
+        return user
 
     def insert_user(self, user: chat.UserLogin) -> bool:
-        sql_statement = "INSERT INTO Users" \
-                        "(user_id, user_name, password)" \
-                        "VALUES" \
-                        "(%(id), %(name), %(password))"
+        sql_statement = "INSERT INTO Users (user_id, user_name, password) VALUES (%s, %s, %s)"
         # user_id
-        user_name = user.username
+        user_name = user.userName
         password = self.hash_password(user.password)
         # rest is default or set later on
 
-        prepared_statements = {"id": "user_id", "name": user_name, "password": password}
+        prepared_statements = ("user_id", user_name, password)
         try:
-            self.cursor().execute(sql_statement, prepared_statements)
+            test = self.cursor.execute(sql_statement, prepared_statements)
+            print(test)
             return True
         except Exception:
             return False
 
     def get_password_by_user_id(self, user_id: str):
-        sql_statement = "SELECT password FROM Users WHERE user_id = %(id)"
-        prepared_statements = {"id": user_id}
-        self.cursor().execute(sql_statement, prepared_statements)
-        password_list = self.cursor.fetchone()
+        sql_statement = "SELECT password FROM Users WHERE user_id = %s"
+        prepared_statements = (user_id,)
+        db_query = self.cursor.execute(sql_statement, prepared_statements)
+        password_list = db_query.fetchone()
         return password_list[0]
 
     def get_password_by_user_name(self, user_name: str):
-        sql_statement = "SELECT password FROM Users WHERE user_name = %(name)"
-        prepared_statements = {"name": user_name}
-        self.cursor().execute(sql_statement, prepared_statements)
-        password_list = self.cursor.fetchone()
+        sql_statement = "SELECT password FROM Users WHERE user_name = %s"
+        prepared_statements = (user_name,)
+        db_query = self.cursor.execute(sql_statement, prepared_statements)
+        password_list = db_query.fetchone()
         return password_list[0]
 
     def hash_password(self, password: str):
@@ -60,7 +59,7 @@ class DBConnector:
         return hashed_password
 
     def compare_passwords(self, given_password: str, user_name: str):
-        hashed_user_password = self.get_password_by_user_name(user_name)
+        hashed_user_password = bytes(self.get_password_by_user_name(user_name), "utf-8")
         bytes_given_password = bytes(given_password, "utf-8")
 
         is_password_equal = bcrypt.checkpw(bytes_given_password, hashed_user_password)
